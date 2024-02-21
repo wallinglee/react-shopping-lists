e6239b430f74f5b790e296e00bf2f02154563f28
Lee Walling <wallinglee@yahoo.com>
2024-02-21 18:00:22 -0500

    update form styles, add store data

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
     const newList = {
       "store": newListTitle,
       "completed": false,
-      "expanded": true,
+      "expanded": false,
       "items": []
     };
 
