f551658fe9004d2e091a49da90c6f4c453667157
Lee Walling <wallinglee@yahoo.com>
2024-03-27 17:39:03 -0400

    item draggable

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,7 @@ export const ListItem = ({ item, toggleItemComplete, deleteItem, list }) => {
     <li
       key={item.id}
       className={"list-item" + (item.completed ? " completed" : "")}
+      draggable
     >
       <p onClick={() => toggleItemComplete(list, item.id)}>{item.text}</p>
       {item.completed && (
